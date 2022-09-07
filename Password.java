a90a116375836fbb438efa9abc8616c114397440
Sayalee Thakur <86339520+sayaleethakur@users.noreply.github.com>
2022-09-07 18:18:38 +0530

    Update Password.java

--- Password.java
+++ Password.java
@@ -1,107 +1,28 @@
-public class Password {
- 
-    // whether a password is valid or not
-    public static boolean isValid(String password)
+class Main
+{
+   
+    public static String generateRandomPassword(int len)
     {
+      
+        final String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
  
-        // for checking if password length
-        // is between 8 and 15
-        if (!((password.length() >= 8)
-              && (password.length() <= 15))) {
-            return false;
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder();
+ 
+        
+ 
+        for (int i = 0; i < len; i++)
+        {
+            int randomIndex = random.nextInt(chars.length());
+            sb.append(chars.charAt(randomIndex));
         }
  
-        // to check space
-        if (password.contains(" ")) {
-            return false;
-        }
-        if (true) {
-            int count = 0;
- 
-            // check digits from 0 to 9
-            for (int i = 0; i <= 9; i++) {
- 
-                // to convert int to string
-                String str1 = Integer.toString(i);
- 
-                if (password.contains(str1)) {
-                    count = 1;
-                }
-            }
-            if (count == 0) {
-                return false;
-            }
-        }
- 
-         if (!(password.contains("@") || password.contains("#")
-              || password.contains("!") || password.contains("~")
-              || password.contains("$") || password.contains("%")
-              || password.contains("^") || password.contains("&")
-              || password.contains("*") || password.contains("|"))) {
-            return false;
-        }
- 
-        if (true) {
-            int count = 0;
- 
-            // checking capital letters
-            for (int i = 65; i <= 90; i++) {
- 
-                // type casting
-                char c = (char)i;
- 
-                String str1 = Character.toString(c);
-                if (password.contains(str1)) {
-                    count = 1;
-                }
-            }
-            if (count == 0) {
-                return false;
-            }
-        }
- 
-        if (true) {
-            int count = 0;
- 
-            // checking small letters
-            for (int i = 97; i <= 122; i++) {
- 
-                // type casting
-                char c = (char)i;
-                String str1 = Character.toString(c);
- 
-                if (password.contains(str1)) {
-                    count = 1;
-                }
-            }
-            if (count == 0) {
-                return false;
-            }
-        }
- 
-        // if all conditions fails
-        return true;
+        return sb.toString();
     }
  
-    // Driver code
     public static void main(String[] args)
     {
- 
-        String password1 = "Sayalee$1";
- 
-        if (isValid(password1)) {
-            System.out.println(password1 + " - Valid Password");
-        }
-        else {
-            System.out.println(password1 + " - Invalid Password!");
-        }
- 
-        String password2 = "SAYALEE1%";
-        if (isValid(password2)) {
-            System.out.println(password2 + " - Valid Password");
-        }
-        else {
-            System.out.println(password2 + " - Invalid Password!");
-        }
+        int len = 10;
+        System.out.println(generateRandomPassword(len));
     }
 }
